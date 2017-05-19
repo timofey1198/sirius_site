0aededcae8612b0e35e8f6ffd6eb949ebe835e13
timofey1198 <timostar98@gmail.com>
2017-05-19 18:06:31 +0300

    Добавлена функция is_access(user, key) для проверки доступа пользователя 0 - админ

--- data.py
+++ data.py
@@ -2,6 +2,19 @@ import openpyxl
 from random import choice, randint
 from PIL import Image, ImageDraw, ImageFont
 
+def is_access(user, key):
+    wb = openpyxl.load_workbook(filename = 'resources/users.xlsx')
+    sheet = wb['1']
+    users_number = sheet['A1'].value
+    for i in range(users_number):
+        u = sheet['A%s'%str(i+3)].value
+        k = sheet['C%s'%str(i+3)].value
+        if u == user and key >= k:
+            wb.save('resources/users.xlsx')
+            return True
+    wb.save('resources/users.xlsx')
+    return False
+
 def is_user(user):
     wb = openpyxl.load_workbook(filename = 'resources/users.xlsx')
     sheet = wb['1']
@@ -34,6 +47,7 @@ def new_user(user, password):
     users_number = sheet['A1'].value
     sheet['A%s' %str(users_number + 3)] = user
     sheet['B%s' %str(users_number + 3)] = password
+    sheet['C%s' %str(users_number + 3)] = 3
     sheet['A1'] = users_number + 1
     wb.save('resources/users.xlsx')
 
@@ -50,3 +64,6 @@ def captcha():
     draw.text( (0,0), key, 0, font)
     img.save('resources/img/captcha/%s.jpg' %str(key), 'JPEG')
     return key
+
+if __name__ == "__main__":
+    print(is_access('timostar', 0))
