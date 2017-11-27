f3ea9aa49bc767a11749921ae846e79a08211ef5
Timofey <timostar98@gmail.com>
2017-11-27 22:06:04 +0300

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,6 +3,6 @@ import smtplib
 def Mail(to, message):
     smtp_obj = smtplib.SMTP('smtp.gmail.com', 587)
     print(smtp_obj.starttls())
-    smtp_obj.login('timostar98@gmail.com', 'Upiter98112358132134')
-    smtp_obj.sendmail('timostar98@gmail.com', to, message)
+    smtp_obj.login('xxx@ccc.xx', 'pass')
+    smtp_obj.sendmail('xxx@ccc.xx', to, message)
     smtp_obj.quit()
